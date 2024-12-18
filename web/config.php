38c33949186df77cd9bb3af3c03df9884920d1be
Beerendonk,Berend B. <b.beerendonk@student.fontys.nl>
2024-12-18 02:25:07 +0100

    allerlaatste dan

--- web/config.php
+++ web/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', getenv('DB_HOST') ?: 'ecommerce-db:30081'); // K8s service naam
+define('DB_HOST', getenv('DB_HOST') ?: 'ecommerce-db'); // K8s service naam
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_NAME', getenv('DB_NAME'));
