4fd624bbf37caf75f6458c32854d6592093f963c
Beerendonk,Berend B. <b.beerendonk@student.fontys.nl>
2024-12-18 01:56:58 +0100

    versie terug config map

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', '192.168.151.241'); // IP van je k3s master node
+define('DB_HOST', getenv('DB_HOST'));
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'ecommerce_password'); // Pas aan naar je echte wachtwoord
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_NAME', 'ecommerce');
 ?>
